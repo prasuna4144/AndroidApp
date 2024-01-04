ea55d18cbede7dcd2837d33675f861608d26d458
prasuna4144 <prasunachandrika13@gmail.com>
2024-01-04 16:34:39 +0530

    weatherapp

---
+++ app/src/main/java/com/example/weatherapp/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.weatherapp
+
+import android.content.Intent  // Import Intent class
+import android.os.Bundle
+import android.os.Handler  // Import Handler class
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash2)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
